ecdf62d8338e6ecc4ddb5c56b388581c2ee6c930
Collin Neumann <s84599@htw-dresden.de>
2023-06-17 11:05:42 +0200

    removed django.conf.urls.url for Django 4.0 support

--- src/aemter/urls.py
+++ src/aemter/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf.urls import include, url
+#from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 
